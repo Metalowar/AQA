b464016c541eb9b0eb844a16338b93b22f10c1b3
Andrii Markin <metalowar@gmail.com>
2024-09-18 18:49:11 +0300

    HW for Lesson 8. First 3 tasks

---
+++ lesson_8/task_2.js
@@ -0,0 +1,3 @@
+const originArray = [1, 2, 3, 4, 5];
+const resultArr = originArray.map((element, index) => element * index);
+console.log(resultArr);
---
+++ lesson_8/task_3.js
@@ -0,0 +1,3 @@
+const arrayNum = [10, 20, 30, 40, 50];
+const arraySum = arrayNum.reduce((previousValue, currentValue) => previousValue + currentValue);
+console.log(arraySum);
