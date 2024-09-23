13ea5139eaf09b772b333915d754a3701029b388
Andrii Markin <metalowar@gmail.com>
2024-09-23 12:06:15 +0300

    HW for Lesson 8. delete extra brackets

--- lesson_8/task_4.js
+++ lesson_8/task_4.js
@@ -1,4 +1,4 @@
 // Відбір парних чисел
 const allNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const evenNumbers = allNumbers.filter((element => element % 2 === 0));
+const evenNumbers = allNumbers.filter(element => element % 2 === 0);
 console.log(evenNumbers);
