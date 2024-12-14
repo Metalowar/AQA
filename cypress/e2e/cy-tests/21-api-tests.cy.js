61a2a3a21845574853d7dc428affa1b46553870f
Andrii Markin <metalowar@gmail.com>
2024-12-14 23:51:47 +0200

    HW 21. Create custom command and finish all tests

--- cypress/e2e/cy-tests/21-api-tests.cy.js
+++ cypress/e2e/cy-tests/21-api-tests.cy.js
@@ -1,8 +1,9 @@
 import { Garage } from './page-objects/20-garagePage.js';
-import { Fuel } from './page-objects/20-fuelPage.js';
 
 describe("Testing with API", () => {
-  let carId;
+
+  let carId; // Створюю змінну, яку отримаю після створення машини
+
   before("Auth on site",() => {
     cy.visit('https://guest:welcome2qauto@qauto.forstudy.space/');
   });
@@ -19,9 +20,18 @@ describe("Testing with API", () => {
     }).then((response) => {
       expect(response.status).to.equal(200);
     });
-    // cy.visit('https://qauto.forstudy.space/panel/garage');
+
     cy.reload();
   });
+
+  after('Delete testing car', () => {
+    cy.request({
+      method: 'DELETE',
+      url: `https://qauto.forstudy.space/api/cars/${carId}`,
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+    });
+  })
 
   it("Create car", () => {
 
@@ -38,7 +48,28 @@ describe("Testing with API", () => {
     cy.wait('@createCar').then((interception) => {
       expect(interception.response.statusCode).to.equal(201);
       expect(interception.response.body.data).to.have.property('id');
-      carId = interception.response.body.data.id;
+      carId = interception.response.body.data.id; // Отримую значення carId, з яким потім працюватиму
     })
+
+    cy.request({
+      method: 'GET',
+      url: 'https://qauto.forstudy.space/api/cars'
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body.data[0].id).to.equal(carId)
+    });
   });
+
+
+  it('Create expence', () => {
+    const requestBody = {
+      carId: carId,
+      reportedAt: "2024-12-14T00:00:00.000Z",
+      mileage: 201,
+      liters: 20,
+      totalCost: 50,
+    };
+
+    cy.createExpences('https://qauto.forstudy.space/api/expenses', requestBody);
+  })
 });
